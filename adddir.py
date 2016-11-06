4dc6d25e969bf69ba48d8c9fb2c4ca76508533a6
zhaoyunping <pingwaiting@163.com>
2016-11-06 14:39:32 +0800

    适配Python3 网络爬虫

--- adddir.py
+++ adddir.py
@@ -9,15 +9,19 @@ def add_files(n):
     if not os.path.isdir(new_title):
         os.mkdir(new_title)
     else:
-        print ('this dir has existed.')
-        exit(-1) ;
+        print('this dir has existed.')
+        exit(-1);
     os.chdir(new_title)
     for i in range(100):
         file_name = 'ping' + str(i)
-        file=open(file_name,'w')
-        file.close()
+        # file=open(file_name,'w')
+        # # file.write('XJTU')
+        # file.writelines(['XJTU\n','SJTU\n','THU\n','PKU\n'])
+        # file.close()
+        with open(file_name, 'w') as file:
+            file.writelines(['XJTU\n', 'SJTU\n', 'THU\n', 'PKU\n'])
 
 
 if __name__ == '__main__':
     add_files(99)
-    print ('I am so happy.')
+    print('I succeed.')
--- crawler.py
+++ crawler.py
@@ -3,30 +3,35 @@
 
 import os
 import re
-import urllib
+import urllib.request
 
-os.path.join(r'/Users/xujianeng/', r'blog_spider')
-os.mkdir(r'/Users/xujianeng/blog_spider')
+dir_name = os.path.join(r'F:\programming', r'blog_spider')
+if not os.path.isdir(dir_name):
+    os.mkdir(dir_name)
 
 
 def gethtml(url):
-    page = urllib.urlopen(url)
+    page = urllib.request.urlopen(url)
     html = page.read()
     return html
 
 
 def getimg(html):
     img_re = re.compile(r'src="(.+?\.jpg)"')
+    # html = html.encode('GBK')
+    html=str(html, encoding="utf-8")
     imglist = re.findall(img_re, html)
     return imglist
 
 
 blog_html = gethtml('http://canonxu.com/2014/11/Pycon2014@Shanghai/')
-with open(r'/Users/xujianeng/blog_html.html', 'w') as f:
-    f.write(blog_html)
+#with open(r'F:\programming\blog_html.html', 'w') as f:
+f=open(r'F:\programming\blog_html.html', 'wb')
+f.write(blog_html)
+f.close()
 
 blog_imglist = getimg(blog_html)
 x = 0
 for imgurl in blog_imglist:
-    urllib.urlretrieve(imgurl, r'/Users/xujianeng/blog_spider/%s.jpg' % x)
+    urllib.request.urlretrieve(imgurl, r'F:\programming\blog_spider\%s.jpg' % x)
     x += 1
